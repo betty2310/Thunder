a717d43185b8e6f1ca940308551b3ee1ade7a643
betty2310 <huynh.dh2310@gmail.com>
2023-07-10 14:24:22 +0700

    add AC analysis code example

--- cli/Program.cs
+++ cli/Program.cs
@@ -8,17 +8,44 @@ namespace SpiceSimulation
     {
         static void Main(string[] args)
         {
+            // dcAnalysis();
+            acAnanlysis();
+        }
+
+        private static void acAnanlysis()
+        {
+            // Build the circuit
+            var ckt = new Circuit(
+                new VoltageSource("V1", "in", "0", 0.0)
+                    .SetParameter("acmag", 1.0),
+                new Resistor("R1", "in", "out", 10.0e3),
+                new Capacitor("C1", "out", "0", 1e-6)
+            );
+
+            var ac = new AC("AC 1", new DecadeSweep(1e-2, 1.0e3, 5));
+
+            var exportVoltage = new ComplexVoltageExport(ac, "out");
+
+            ac.ExportSimulationData += (sender, args) =>
+            {
+                var output = exportVoltage.Value;
+                var decibels = 10.0 * Math.Log10(output.Real * output.Real + output.Imaginary * output.Imaginary);
+                Console.WriteLine($"decibels:{decibels}V - output:{output}V");
+            };
+            ac.Run(ckt);
+        }
+
+        private static void dcAnalysis()
+        {
             Resistor re = new Resistor("R1", "in", "out", 1.0e4);
             // Build the circuit
             var ckt = new Circuit(
-                     new VoltageSource("V1", "in", "0", 1.0),
-
-                     new Resistor("R2", "out", "0", 2.0e4)
+                new VoltageSource("V1", "in", "0", 1.0),
+                new Resistor("R2", "out", "0", 2.0e4)
             );
 
             ckt.Add(re);
 
-            // Create a DC simulation that sweeps V1 from -1V to 1V in steps of 100mV
             var dc = new DC("DC 1", "V1", -1.0, 1.0, 0.5);
 
             // Catch exported data
@@ -29,7 +56,6 @@ namespace SpiceSimulation
                 Console.WriteLine($"input:{input}V - output:{output}V");
             };
             dc.Run(ckt);
-
         }
     }
 }
